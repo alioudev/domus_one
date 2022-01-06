6ba107994f81eda7f6a348727a4bab123cf52b1c
aliou@aliou.dev <aliou@aliou.dev>
2022-01-07 00:56:46 +0100

    Lecture de fichiers fonctionne
    - Modifications qui seront validées :
    	nouveau fichier : .idea/.gitignore
    	nouveau fichier : .idea/aws.xml
    	nouveau fichier : .idea/misc.xml
    	nouveau fichier : .idea/modules.xml
    - Modifications qui ne seront pas validées :
    	modifié :         base_cup.cup

---
+++ CMaisonUser.java
@@ -0,0 +1,15 @@
+ public class CMaisonUser extends CMaison { 
+	 public CMaisonUser() { 
+	 	 super(); 
+ 
+ 
+
+
+ 
+ 
+ 	 	 monHabitat = new HabitatSpecific(ma_liste_appareils, 
+	 	 	 ma_liste_ens_appareils, ma_liste_scenarios,
+	 	 	 	 ma_liste_interfaces, ma_liste_programmations);
+	 }
+}
+
